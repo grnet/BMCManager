fbe0942cac9e4226d28d5daa2b17d30e7e372fff
Aggelos Kolaitis <neoaggelos@gmail.com>
2020-07-05 21:15:22 +0300

    Add `bmcmanager ipmitool` command

---
+++ bmcmanager/commands/ipmitool.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2020  GRNET S.A.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from bmcmanager.commands.base import BMCManagerServerCommand
+
+
+class Run(BMCManagerServerCommand):
+    """
+    Run ipmitool
+    """
+    oob_method = 'ipmitool'
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.add_argument('args', default=[], nargs='*')
+        return parser
