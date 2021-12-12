105cee35122e6252d7486daf270159a098e881a5
Angelos Kolaitis <neoaggelos@gmail.com>
2021-12-12 20:33:45 +0200

    Document configuration options and add example config file

--- bmcmanager/config.py
+++ bmcmanager/config.py
@@ -26,38 +26,137 @@ LOG = logging.getLogger(__name__)
 CONF = cfg.ConfigOpts()
 
 DEFAULT_OPTIONS = [
-    cfg.ListOpt(name="available_dcims", default=[]),
-    cfg.StrOpt(name="default_dcim"),
-    cfg.StrOpt(name="fallback_oob", default="base"),
-    cfg.DictOpt(name="oob_overrides", default={}),
+    cfg.ListOpt(
+        name="available_dcims",
+        default=[],
+        help="List of configured DCIMs. For each DCIM, create a new [$dcim] section"
+        "and configure options from the `dcim_options` group.",
+        sample_default="netbox, maas",
+    ),
+    cfg.StrOpt(
+        name="default_dcim",
+        help="Name of DCIM to use when none is specified in the command line",
+        sample_default="netbox",
+    ),
+    cfg.StrOpt(
+        name="fallback_oob",
+        default="base",
+        help="Default OOB implementation to use if manufacturer is not supported.",
+        sample_default="base",
+    ),
+    cfg.DictOpt(
+        name="oob_overrides",
+        default={},
+        help="Mapping of OOB implementation overrides.",
+        sample_default="hp:base",
+    ),
 ]
 
 CONF.register_opts(DEFAULT_OPTIONS)
 
 DCIM_OPTIONS = [
-    cfg.StrOpt(name="type", choices=ExtensionManager("bmcmanager.dcim").names()),
+    cfg.StrOpt(
+        name="type",
+        choices=ExtensionManager("bmcmanager.dcim").names(),
+        help="Type of DCIM. Depending on the type you choose, configure the "
+        "appropriate options below",
+        sample_default="netbox",
+    ),
     # NetBox
-    cfg.URIOpt(name="netbox_url", schemes=["http", "https"]),
-    cfg.StrOpt(name="netbox_api_token", secret=True),
-    cfg.IntOpt(name="netbox_api_timeout", default=10),
-    cfg.StrOpt(name="netbox_session_key", secret=True),
-    cfg.StrOpt(name="netbox_credentials_secret"),
-    cfg.ListOpt(name="netbox_device_type_ids", item_type=int),
+    cfg.URIOpt(
+        name="netbox_url",
+        schemes=["http", "https"],
+        help="Root URL of your NetBox deployment",
+        sample_default="https://netbox.example.com/",
+    ),
+    cfg.StrOpt(
+        name="netbox_api_token",
+        secret=True,
+        help="Token for NetBox API requests. May not be needed if NetBox does "
+        "not require authentication for read-only access",
+        sample_default="2eaf86dcfec8f43d59a7ae89d337ebb3fe7fe4e3",
+    ),
+    cfg.IntOpt(
+        name="netbox_api_timeout",
+        default=10,
+        help="Timeout for NetBox API calls (in seconds)",
+    ),
+    cfg.StrOpt(
+        name="netbox_session_key",
+        secret=True,
+        help="Session Key for retrieving secrets from the NetBox API. This is "
+        "needed when using secrets for IPMI credentials",
+        sample_default="Se/wfMUmhfq5el3XA8asorsfArQ=",
+    ),
+    cfg.StrOpt(
+        name="netbox_credentials_secret",
+        help="Name of secret which contains IPMI credentials for the server",
+        sample_default="ipmi-credentials",
+    ),
+    cfg.ListOpt(
+        name="netbox_device_type_ids",
+        item_type=int,
+        help="Limit requests to specific device type IDs.",
+        sample_default="[1, 32]",
+    ),
     # MaaS
-    cfg.URIOpt(name="maas_api_url", schemes=["http", "https"]),
-    cfg.StrOpt(name="maas_api_key", secret=True),
-    cfg.URIOpt(name="maas_ui_url", schemes=["http", "https"]),
+    cfg.URIOpt(
+        name="maas_api_url",
+        schemes=["http", "https"],
+        help="MaaS API URL",
+        sample_default="http://maas:5240/MAAS/api/2.0",
+    ),
+    cfg.StrOpt(name="maas_api_key", secret=True, help="MaaS API Key"),
+    cfg.URIOpt(
+        name="maas_ui_url",
+        schemes=["http", "https"],
+        help="URL of MaaS console.",
+        sample_default="http://maas:5240/",
+    ),
     # Local
-    cfg.HostAddressOpt(name="local_ipmi_address"),
+    cfg.HostAddressOpt(
+        name="local_ipmi_address",
+        help="Specify the IPMI address of the current server",
+        sample_default="1.1.1.1",
+    ),
 ]
 
 OOB_OPTIONS = [
-    cfg.StrOpt("username"),
-    cfg.StrOpt("password", secret=True),
-    cfg.StrOpt("http_share"),
-    cfg.StrOpt("nfs_share"),
-    cfg.IntOpt("expected_psus", default=1, min=0),
-    cfg.DictOpt("expected_firmware_versions", default={}),
+    cfg.StrOpt(
+        "username",
+        help="Default IPMI username. Will be used if no IPMI credentials are found "
+        "from the DCIM",
+        sample_default="admin",
+    ),
+    cfg.StrOpt(
+        "password",
+        secret=True,
+        help="Default IPMI username. Will be used if no IPMI credentials are found "
+        "from the DCIM",
+        sample_default="password",
+    ),
+    cfg.URIOpt(
+        "http_share",
+        schemes=["http"],
+        help="HTTP Share (currently only used by Dell servers)",
+        sample_default="http://10.0.0.1/",
+    ),
+    cfg.HostAddressOpt(
+        "nfs_share", help="NFS Share (currently only used by Dell servers)"
+    ),
+    cfg.IntOpt(
+        "expected_psus",
+        default=1,
+        min=0,
+        help="Number of PSUs expected on the server",
+        sample_default=2,
+    ),
+    cfg.DictOpt(
+        "expected_firmware_versions",
+        default={},
+        help="Mapping of expected firmware versions for this server.",
+        sample_default="bios:1.0.0, tsm:1.0.0, psu_$model=1.3.2",
+    ),
 ]
 
 
@@ -101,3 +200,11 @@ def load_config(args: argparse.Namespace, OOBS):
         CONF.log_opt_values(LOG, logging.DEBUG)
 
     return CONF
+
+
+def list_opts():
+    return [
+        ("", DEFAULT_OPTIONS),
+        ("dcim", DCIM_OPTIONS),
+        ("oob", OOB_OPTIONS),
+    ]
